07acae78fd705da58e1e266a15e3aa66b50a9216
Tamago0926 <torigoe-ch@outlook.jp>
2025-05-16 22:58:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ const p_number = participant.length -1;
 for(i = 0;i <= p_number;i++){
     document.getElementById("participant").insertAdjacentHTML("afterend",`<div id="Participants">・${participant[i]}</div>`);
 }
+
+//ドロワー処理
+const hum = document.getElementById("hum_img");
+const dorowar = document.getElementById("left_dorowar");
+const d_text = document.getElementById("dorowar_text");
+
+hum.addEventListener("click", () => {
+    hum.classList.toggle("hum_open");
+    dorowar.classList.toggle("dorowar_open");
+    d_text.classList.toggle("text_open")
+})
