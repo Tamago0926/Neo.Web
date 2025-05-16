245f2ac282d204f8fe818a1ff240516a3e2d4445
Tamago0926 <torigoe-ch@outlook.jp>
2025-05-16 18:39:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 "use-strict";
 
-const notice = ["夏休み中に大会を開きます","Online Tetris Neoの大会記録用のWebページを作りました"];
-const notice_n = notice.length - 1;
-console.log(notice_n);
+//お知らせ
+const notice = ["夏休み中に大会を開きます","Online Tetris Neoの大会記録用のWebページを作りました","Hello World"];
+const n_number = notice.length - 1;
 
-for( i = 0;i <= notice_n;i++){
+for(i = 0;i <= n_number;i++){
     document.getElementById("notice").insertAdjacentHTML("afterend", `<div id="notices">・${notice[i]}</div>`);
 }
+
+//参加者
+const participant = ["tamago0926","tamago1","tamago2"];
+const p_number = participant.length -1;
+for(i = 0;i <= p_number;i++){
+    document.getElementById("participant").insertAdjacentHTML("afterend",`<div id="Participants">・${participant[i]}</div>`);
+}
+
+let request = new XMLHttpRequest();
+
+request.open("GET","https://api.scratch.mit.edu/users/tamago0926/following/studios/projects",true);
+request.onload = function(){
+    let date = this.request;
+    console.log(date);
+}
+
+request.send();
